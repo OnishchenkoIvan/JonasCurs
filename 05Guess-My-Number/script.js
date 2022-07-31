646b56570fa76bd51fa0e93dd8e8e2ad969a5da8
Ivan Onishchenko <Lightsout07@mail.ru>
2022-07-31 22:49:50 +0300

    075 Manipulating CSS styles

--- 05Guess-My-Number/script.js
+++ 05Guess-My-Number/script.js
@@ -20,6 +20,10 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'NO number!';
   } else  if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number!';
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem'
   } else if (guess > secretNumber) {
     if (score > 1) {
     document.querySelector('.message').textContent = 'Too high!';
