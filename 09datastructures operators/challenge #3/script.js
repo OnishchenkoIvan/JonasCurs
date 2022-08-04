77812338de6ce853e6edea7c2d70f22c005708c2
Ivan Onishchenko <Lightsout07@mail.ru>
2022-08-04 11:16:56 +0300

    120 challenge #3 end

--- 09datastructures operators/challenge #3/script.js
+++ 09datastructures operators/challenge #3/script.js
@@ -11,3 +11,28 @@ const gameEvents = new Map([
   [80, '⚽️ GOAL'],
   [92, '🔶 Yellow card'],
 ]);
+
+// #1
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+// #2
+// gameEvents.delete(64);
+// console.log(gameEvents);
+
+// #3
+const arr = gameEvents.values();
+const result = {};
+for (const ar of arr) {
+  result[ar] ? result[ar] += 1 : result[ar] = 1;
+}
+for (const [key, value] of Object.entries(result)) {
+  console.log(`An ${key} happened, on average, every ${90 / value} minutes`);
+}
+
+// #4
+
+for (const [key, value] of gameEvents.entries()) {
+  const str = key <= 45 ? `[First Half] ${key}: ${value}` : `[Second Half] ${key}: ${value}`;
+  console.log(str);
+}
