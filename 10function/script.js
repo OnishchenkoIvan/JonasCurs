c7e64641a2b7999da67b059411646e7b6f8a868d
Ivan Onishchenko <Lightsout07@mail.ru>
2022-08-05 12:45:44 +0300

    133 the call and apply methods

--- 10function/script.js
+++ 10function/script.js
@@ -14,3 +14,46 @@ greet('Hello')('Jonas');
 
 const greetArrow = greeting => name => console.log(`${greeting} ${name}`);
 greetArrow("Hi")("Jonas");
+
+const lufthansa = {
+  airline: 'Lufthansa',
+  dataCode: 'LH',
+  bookings: [],
+  book(flightNum, name) {
+    console.log(`${name} booked a seat on ${this.airline} flight ${this.dataCode}${flightNum}`);
+    this.bookings.push({flight: `${this.dataCode}${flightNum}, name`});
+  },
+};
+
+lufthansa.book(239, 'Jonas Schmedtmann');
+lufthansa.book(635, 'John Smith');
+
+const eurowings = {
+  airline: 'Eurowings',
+  dataCode: 'EW',
+  bookings: [],
+};
+
+const book = lufthansa.book;
+
+// book(23, 'Sarah Williams');
+book.call(eurowings, 23, 'Sarah Williams');
+console.log(eurowings);
+
+book.call(lufthansa, 239, 'Mary Cooper');
+console.log(lufthansa);
+const swiss = {
+  airline: 'Swiss Air Lines',
+  dataCode: 'LX',
+  bookings: []
+}
+
+book.call(swiss, 583, 'Mary Cooper');
+console.log(swiss);
+
+// Apply method
+const flightData = [583, 'George Cooper'];
+book.apply(swiss, flightData);
+console.log(swiss);
+
+book.call(swiss, ...flightData);
