fbcf703cf6478baa9e566f243ea0bc39a70cfe1b
Ivan Onishchenko <Lightsout07@mail.ru>
2022-08-05 08:36:21 +0300

    132 functions returning functions

--- 10function/script.js
+++ 10function/script.js
@@ -1,2 +1,16 @@
 'use strict';
 
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  }
+}
+
+const greeterHey = greet('Hey');
+greeterHey('Jonas');
+greeterHey('Steven');
+
+greet('Hello')('Jonas');
+
+const greetArrow = greeting => name => console.log(`${greeting} ${name}`);
+greetArrow("Hi")("Jonas");
