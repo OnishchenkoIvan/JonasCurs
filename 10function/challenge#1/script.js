91297de44dfb8f21d202d81f3539e946c01e5e12
Ivan Onishchenko <Lightsout07@mail.ru>
2022-08-05 15:19:50 +0300

    135 challenge #1 finish

--- 10function/challenge#1/script.js
+++ 10function/challenge#1/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  answers: new Array(4).fill(0),
+  registerNewAnswer () {
+    const options = this.options.join('\n');
+
+    const answer = prompt(`${this.question}\n${options}\n(Write option number)`);
+    if (answer >= 0 && answer < 4) {
+      this.answers[answer]++;
+    } else alert('Put correct answer, please');
+    this.displayResults();
+    this.displayResults('sting');
+  },
+  displayResults () {
+    if (Array.isArray(this.answers)) {
+      console.log(this.answers);
+    } console.log(`Poll results are ${this.answers.join(', ')}`);
+  }
+};
+
+document.querySelector('.poll').addEventListener('click', function () {
+  poll.registerNewAnswer();
+});
+
+poll.displayResults.call({answers: [5, 2, 3]});
