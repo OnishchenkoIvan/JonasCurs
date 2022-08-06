d8020b0d683d6ab1510b0376f1ad9cbf6a75d23a
Ivan Onishchenko <Lightsout07@mail.ru>
2022-08-06 14:48:25 +0300

    148 challenge#1 finish

--- 11WorkingWithArrays/challenge#1/script.js
+++ 11WorkingWithArrays/challenge#1/script.js
@@ -1,2 +1,12 @@
 'use strict';
 
+function checkDogs (dogsJulia, dogsKate) {
+  const realDogs = dogsJulia.slice(1, 3);
+  const arrAdd = [...realDogs, ...dogsKate]
+  arrAdd.forEach(function (item, i) {
+    if (item >= 3) {
+      console.log(`Dog number ${i + 1} is an adult, and is ${item} years old`)
+    } else console.log(`Dog number ${i + 1} is still a puppy 🐶`)
+  })
+}
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3])
