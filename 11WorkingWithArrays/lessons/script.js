bc8d7b8672b277b84e0cd8394434d82f0c7e0356
Ivan Onishchenko <Lightsout07@mail.ru>
2022-08-06 20:52:50 +0300

    157 the find method

--- 11WorkingWithArrays/lessons/script.js
+++ 11WorkingWithArrays/lessons/script.js
@@ -199,3 +199,10 @@ const totalDepositsUSD = movements
   .map(mov => mov * eurToUsd)
   .reduce((acc, mov) => acc + mov, 0);
 console.log(totalDepositsUSD);
+
+const firstWithdrawal = movements.find(mov => mov < 0);
+console.log(firstWithdrawal);
+
+console.log(accounts);
+const account = accounts.find(acc => acc.owner = "Jessica Davis");
+console.log(account);
