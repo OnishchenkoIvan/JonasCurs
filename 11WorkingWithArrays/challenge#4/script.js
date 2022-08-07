1acab41a56c75328951f4e8821164ad6ad00d88b
Ivan Onishchenko <Lightsout07@mail.ru>
2022-08-07 23:02:01 +0300

    167 challenge #4 end

--- 11WorkingWithArrays/challenge#4/script.js
+++ 11WorkingWithArrays/challenge#4/script.js
@@ -1,2 +1,39 @@
 'use strict';
 
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] }
+];
+// 1.
+dogs.map((item) => item.recommendedFood = Math.trunc(item.weight ** 0.75 * 28))
+console.log(dogs)
+
+// 2.
+const dogSarah = dogs.find(dog => dog.owners.includes('Sarah'));
+
+console.log(dogSarah);
+
+// 3.
+const ownersEatTooMuch = dogs.filter(dog => dog.curFood > dog.recommendedFood)
+  .flatMap(dog => dog.owners);
+const ownersEatTooLittle = dogs.filter(dog => dog.curFood < dog.recommendedFood)
+  .flatMap(dog => dog.owners);
+console.log(ownersEatTooLittle, ownersEatTooMuch);
+
+// 4.
+console.log(`${ownersEatTooMuch.join(' and ')}'s dogs eat too much!`);
+console.log(`${ownersEatTooLittle.join(' and ')}'s dogs eat too little!`);
+
+// 5.
+console.log(dogs.some(dog => dog.curFood === dog.recommendedFood));
+
+// 6.
+const checkEatingOkay = dog => dog.curFood > dog.recommendedFood * 0.9 && dog.curFood < dog.recommendedFood * 1.1;
+console.log(dogs.some(checkEatingOkay));
+// 7.
+console.log(dogs.filter(checkEatingOkay));
+// 8.
+const dogsSorted = dogs.slice().sort((a, b) => a.recommendedFood - b.recommendedFood);
+console.log(dogsSorted);
