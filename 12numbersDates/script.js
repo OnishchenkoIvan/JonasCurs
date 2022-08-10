40b9b096b18cbe95882b59b41eede9bede29a950
Ivan Onishchenko <Lightsout07@mail.ru>
2022-08-10 22:51:41 +0300

    179 internationalizing numbers Intl

--- 12numbersDates/script.js
+++ 12numbersDates/script.js
@@ -386,6 +386,18 @@ const calcDaysPassed = (date1, date2) => Math.abs(date2 - date1) / (1000 * 60 * 
 const days1 = calcDaysPassed(new Date(2037, 3, 14), new Date(2037, 3, 24));
 
 console.log(days1);
+const options = {
+  style: 'currency',
+  unit: 'celsius',
+  currency: 'EUR',
+  // useGrouping: false,
+}
+
+const num = 38884764.23;
+console.log('US: ', new Intl.NumberFormat('en-US', options).format(num));
+console.log('Germany: ', new Intl.NumberFormat('de-DE', options).format(num));
+console.log('Syria: ', new Intl.NumberFormat('ar-SY', options).format(num));
+console.log(navigator.language, new Intl.NumberFormat(navigator.language, options).format(num));
 
 
 
