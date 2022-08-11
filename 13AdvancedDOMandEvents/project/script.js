e600d9f1eb61366cfebdfa0c054c19682b210e52
Ivan Onishchenko <Lightsout07@mail.ru>
2022-08-11 12:58:50 +0300

    187 styles, attributes and classes

--- 13AdvancedDOMandEvents/project/script.js
+++ 13AdvancedDOMandEvents/project/script.js
@@ -53,7 +53,7 @@ console.log(document.getElementsByClassName('btn'));
 const message = document.createElement('div');
 message.classList.add('cookie-message');
 // message.textContent = 'We use cookied for improved functionality and analytics.';
-message.innerHTML = 'We use cookied for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</bbutton>';
+message.innerHTML = 'We use cookied for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
 
 // header.prepend(message);
 header.append(message);
@@ -65,4 +65,49 @@ header.append(message);
 document.querySelector('.btn--close-cookie').addEventListener('click', function () {
   // message.remove();
   message.parentElement.removeChild(message);
-})
+});
+
+//Styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+console.log(message.style.color);
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height = Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+//attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.className);
+
+logo.alt = 'Beatiful minimalist logo';
+
+//non-standard
+console.log(logo.designer);
+console.log(logo.getAttribute('designer'));
+logo.setAttribute('company', 'Bankist');
+console.log(logo.src);
+console.log(logo.getAttribute('src'));
+
+const link = document.querySelector('.twitter-link');
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+//Data attributes
+console.log(logo.dataset.versionNumber);
+
+//classes
+logo.classList.add('c', 'j');
+logo.classList.remove('c', 'j');
+logo.classList.toggle('c');
+logo.classList.contains('c'); // not includes
+
+// don't use
+logo.className = 'jonas';
