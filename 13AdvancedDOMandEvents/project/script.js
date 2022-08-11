bbc9da059fbc197a11703bb4876f70a749c3ca1e
Ivan Onishchenko <Lightsout07@mail.ru>
2022-08-11 22:40:10 +0300

    189 Types of Events handlers

--- 13AdvancedDOMandEvents/project/script.js
+++ 13AdvancedDOMandEvents/project/script.js
@@ -138,3 +138,18 @@ btnScrollTo.addEventListener('click', function (e) {
 
   section1.scrollIntoView({behavior: 'smooth'})
 });
+
+const h1 = document.querySelector('h1');
+
+const alertH1 = function (e) {
+  alert('addEventlistener: Great! You are reading the heading :D');
+
+};
+
+h1.addEventListener('mouseenter', alertH1);
+
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+// h1.onmouseenter = function (e) {
+//   alert('addEventlistener: Great! You are reading the heading :1D');
+// };
+
