c1366ff91b32827b4436a5fb36a52cec4fbf9121
Ivan Onishchenko <Lightsout07@mail.ru>
2022-08-12 12:42:54 +0300

    193 dom traversing

--- 13AdvancedDOMandEvents/project/script.js
+++ 13AdvancedDOMandEvents/project/script.js
@@ -168,7 +168,7 @@ logo.className = 'jonas';
 */
 
 
-const h1 = document.querySelector('h1');
+// const h1 = document.querySelector('h1');
 
 const alertH1 = function (e) {
   alert('addEventlistener: Great! You are reading the heading :D');
@@ -203,3 +203,32 @@ document.querySelector('.nav').addEventListener('click', function (e) {
   console.log('nav', e.target, e.currentTarget);
 }, true);
 */
+
+const h1 = document.querySelector('h1');
+
+//Going downwards: child
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
+console.log(h1.children);
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'orangered';
+
+// going upwards: parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
+
+h1.closest('h1').style.background = 'var(--gradient-primary)';
+
+// going sideways: siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach(function (el) {
+  if (el !== h1) el.style.transform = 'scale(0.5)';
+});
