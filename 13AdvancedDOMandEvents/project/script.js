1be916e52ad8786abf1160565c2e6fdb4f8cc568
Ivan Onishchenko <Lightsout07@mail.ru>
2022-08-12 14:13:22 +0300

    194 building a tabbed component

--- 13AdvancedDOMandEvents/project/script.js
+++ 13AdvancedDOMandEvents/project/script.js
@@ -82,7 +82,30 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
       behavior: 'smooth'
     });
   }
-})
+});
+
+// Tabbed component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+  console.log(clicked);
+
+  // Guard clause
+  if(!clicked) return;
+
+  // Active tab
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'))
+  clicked.classList.add('operations__tab--active');
+
+  // Activate content area
+  console.log(clicked.dataset.tab);
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
+});
+
 ///////////////////////////////////////////=========================
 //selecting elements
 /*
@@ -207,6 +230,7 @@ document.querySelector('.nav').addEventListener('click', function (e) {
 const h1 = document.querySelector('h1');
 
 //Going downwards: child
+/*
 console.log(h1.querySelectorAll('.highlight'));
 console.log(h1.childNodes);
 console.log(h1.children);
@@ -232,3 +256,5 @@ console.log(h1.parentElement.children);
 [...h1.parentElement.children].forEach(function (el) {
   if (el !== h1) el.style.transform = 'scale(0.5)';
 });
+ */
+
