256fcb8d1fe1956ca43171b2e948b5c7d1148e74
Ivan Onishchenko <Lightsout07@mail.ru>
2022-08-14 18:37:10 +0300

    213 ES6 Classes

--- 14OOP/script.js
+++ 14OOP/script.js
@@ -70,3 +70,36 @@ console.log(arr.unique());
 
 const h1 = document.querySelector('h1');
 console.dir(x => x + 1);
+
+// class expression
+// const PersonCl = class {}
+
+// class declaration
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+  // Methods will be added to .prototype property
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.firstName}`)
+  }
+}
+
+const jessica = new PersonCl('Jessica', 1996);
+console.log(jessica);
+jessica.calcAge();
+console.log(jessica.__proto__ === PersonCl.prototype);
+//
+// PersonCl.prototype.greet = function () {
+//   console.log(`Hey ${this.firstName}`);
+// }
+jessica.greet();
+
+// 1. Classes are NOT hoisted
+// 2. Class are first-class citizes
+// 3. Classes are executed in strict mode
