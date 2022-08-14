6d7ef28e35d66a9dbc8d99f7a8da895f0278a205
Ivan Onishchenko <Lightsout07@mail.ru>
2022-08-14 21:34:00 +0300

    217 coding Challenge #2

---
+++ 14OOP/challenge#2/script.js
@@ -0,0 +1,33 @@
+'use strict';
+
+class CarCl {
+  constructor(mark, speed) {
+    this.mark = mark;
+    this.speed = speed;
+  }
+
+  addSpeed() {
+    this.speed += 10;
+    console.log(`${this.mark} is going at ${this.speed} km/h`);
+  };
+  lowSpeed() {
+    this.speed -= 5;
+    console.log(`${this.mark} is going at ${this.speed} km/h`);
+  }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+
+  set speedUS(speed) {
+  this.speed = speed * 1.6;
+  }
+}
+
+const ford = new CarCl('Ford', 120);
+console.log(ford.speedUS);
+ford.addSpeed();
+ford.addSpeed();
+ford.lowSpeed();
+ford.speedUS = 50;
+console.log(ford)
