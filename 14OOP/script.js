3f4869ab980412441f67ce5e92c8f012eb93d156
Ivan Onishchenko <Lightsout07@mail.ru>
2022-08-15 12:19:57 +0300

    220 inheritance between classes

--- 14OOP/script.js
+++ 14OOP/script.js
@@ -167,6 +167,8 @@ sarah.calcAge();
 
  */
 ////////////////////////////////218
+
+
 const Person = function (firstName, birthYear) {
   // Instance properties
   this.firstName = firstName;
@@ -177,6 +179,41 @@ const Person = function (firstName, birthYear) {
   //   console.log(2037 - this.birthYear);
   // };
 };
+
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+  // Methods will be added to .prototype property
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.firstName}`)
+  }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  // Set a property that already exists
+  set fullName(name) {
+    console.log(name);
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name!`)
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+
+  static  hey() {
+    console.log('hey there111');
+    console.log(this);
+  }
+}
 
 
 Person.prototype.calcAge = function () {
@@ -209,3 +246,27 @@ console.log(mike instanceof Object);
 
 Student.prototype.constructor = Student;
 console.log(Student.prototype.constructor)
+
+
+
+
+class StudentCl extends PersonCl {
+  constructor(fullName, birthYear, course) {
+    //Always needs to happen first!
+    super(fullName, birthYear)
+    this.course = course;
+  }
+
+  introduce() {
+    console.log(`My name is ${this.fullName} and I stydy ${this.course}`);
+  }
+
+  calcAge() {
+    console.log(`I'm ${2037 - this.birthYear} eyars old, but as a student I feel more like ${2037 - this.birthYear + 10}`)
+  }
+}
+
+const martha = new StudentCl('Martha Jones', 2012, 'Computer Science');
+martha.introduce();
+martha.calcAge();
+
